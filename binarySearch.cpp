cbc8acdc1b003193f3e797a82f40dd17e3fd4d50
3rectangles <99025657+3rectangles@users.noreply.github.com>
2022-04-13 01:27:45 +0530

    Update binarySearch.cpp

--- binarySearch.cpp
+++ binarySearch.cpp
@@ -59,4 +59,65 @@ public:
         return NumberOfHours <= h;
     }
 };
-xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
+xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx search in reverse sorted array xxxxxxxxxxxxxxxxxxxx
+int l=0;
+int r= v.size()-1;
+int key; // tosearch
+// 29  18 15 14 11  8 6 4 3 1 0
+while(l<=r)
+{
+ int mid = l + (r-L)/2;
+ if(v[mid] == key ) return mid; 
+ else if (v[mid] > target) l = mid+1;
+ else 
+  r = mid - 1;
+}
+return -1 ; // not found
+xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx  First and Last occurrence of an Element xxxxxxxxxxxxxxxx
+// using stl 
+int l = lower_bound(v.begin(), v.end(), key) - v.begin();   // index at which elmn isnt less than target
+int r = upper_bound(v.begin(), v.end(), key-1) - v.begin(); // strictly greater than target
+if(l ==n) // not found
+  return -1;
+if( v[l] != key) // target not found
+  return -1; 
+else // first occurance
+int  f= l;
+// if we reach till here then , key is present
+if(r == n ) break; // not possible
+
+lastocc=r;
+xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
+int firstocc( int l , int r, int k)
+{
+  int f=-1;
+  while(l<=r)
+     {
+      int mid = l + (r-L)/2;
+      if(v[mid] == k ) // can be first occurance but we cont our search to left side
+      {  f= mid; r= mid-1; }
+      else if (v[mid] > k) l = mid+1;
+      else 
+       r = mid - 1;
+     }
+  return f ; // not found
+}
+
+int lastocc( int l , int r, int k)
+{
+  int lo=-1;
+  while(l<=r)
+     {
+      int mid = l + (r-L)/2;
+      if(v[mid] == k ) // can be possible last occurance but we cont our search to righ side
+      {  lo= mid; l= mid-1; }
+      else if (v[mid] > k) l = mid+1;
+      else 
+       r = mid - 1;
+     }
+  return lo ; // not found
+}
+xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
+
+
+
