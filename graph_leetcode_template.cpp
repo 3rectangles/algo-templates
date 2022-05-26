7a062bcbd698ab8e9e8391d1312e8af74c8cc327
3rectangles <99025657+3rectangles@users.noreply.github.com>
2022-05-26 23:17:33 +0530

    Update graph_leetcode_template.cpp
    bell mon ford, at max k edges

--- graph_leetcode_template.cpp
+++ graph_leetcode_template.cpp
@@ -102,4 +102,77 @@ bool dfs( int u, int par =-1, int c=0)
     
         return true; // every chain of child was bipartite
     }
-xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
+xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx BELMON ford xxxxxxxxxxxxxxx
+//shortest path at max k +1 edges 
+class Solution
+{
+public:
+    
+    int findCheapestPrice(int n, vector<vector<int>>& flights, int src, int dst, int K)
+    {        
+        vector<vector<int>> dp(K+2, vector<int>(n, INT_MAX));
+        
+        //dp[i][j] = Dist to reach j using atmost i edges from src
+        
+        for(int i = 0; i <= K+1; i++)
+        {
+            dp[i][src] = 0; // Dist to reach src always zero
+        }
+        
+        for(int i = 1; i <= K+1; i++)
+        {
+            for(auto &f: flights)
+            {
+                //Using indegree
+                int u = f[0];
+                int v = f[1];
+                int w = f[2];
+                
+                if(dp[i-1][u] != INT_MAX)
+                    dp[i][v] = min(dp[i][v], dp[i-1][u] + w);
+            }
+        }
+        
+        return (dp[K+1][dst] == INT_MAX)? -1: dp[K+1][dst];
+    }
+};
+
+
+
+
+// space optimized
+
+class Solution
+{
+public:
+    
+    int findCheapestPrice(int n, vector<vector<int>>& flights, int src, int dst, int K)
+    {        
+        vector<vector<int>> dp(2, vector<int>(n, INT_MAX));
+        
+        //dp[i][j] = Dist to reach j using atmost i edges from src
+        
+        for(int i = 0; i <= K+1; i++)
+        {
+            dp[i%2][src] = 0; // Dist to reach src always zero
+        }
+        
+        for(int i = 1; i <= K+1; i++)
+        {
+            for(auto &f: flights)
+            {
+                //Using indegree
+                int u = f[0];
+                int v = f[1];
+                int w = f[2];
+                
+                if(dp[ (i-1 )  %2][u] != INT_MAX)
+                    dp[i %2][v] = min(dp[i%2][v], dp[(i-1 ) %2][u] + w);
+            }
+        }
+        
+        return (dp[(K+1) %2][dst] == INT_MAX)? -1: dp[(K+1) %2][dst];
+    }
+};
+
+xxxxxxxxxxxxxxxxxxxxxxxxxxx
